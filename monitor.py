2abf2f568040863eb76931d51a8b9a6d99dbdf41
Max von Webel <max@343max.de>
2021-03-19 15:24:27 +0000

    added some very stupid monitoring

--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@
 
 import requests
 import configparser
+import json
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
@@ -14,5 +15,34 @@ def send_message(message):
     response = requests.get(url)
     return response.json()
 
-response = send_message('Hello!')
-print(response)
+# response = send_message('Hello!')
+# print(response)
+
+def check_server(url):
+  try:
+    response = requests.get(url, timeout=0.5)
+    return response.status_code == 200
+  except requests.exceptions.Timeout:
+    return False
+
+state_json = 'state.json'
+url = 'https://343max.de/'
+
+try:
+  with open(state_json) as json_file:
+    last_reachable = json.load(json_file)
+except FileNotFoundError:
+  last_reachable = False
+except json.decoder.JSONDecodeError:
+  last_reachable = False
+
+current_reachable = check_server(url)
+
+if last_reachable != current_reachable:
+  if current_reachable == True:
+    send_message('{url} is reachable again'.format(url=url))
+  else:
+    send_message('{url} is not reachable'.format(url=url))
+
+  with open(state_json, 'w') as json_file:
+    json.dump(current_reachable, json_file)
